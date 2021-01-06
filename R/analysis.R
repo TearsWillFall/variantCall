4c65fbf19515308ff70c516030f33db3ab9e18de
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-01-06 15:52:02 +0000

    Fix issue with pbapply and dimensionless data

--- R/analysis.R
+++ R/analysis.R
@@ -158,7 +158,7 @@ vcf_mutect2_parallel=function(bin_path="tools/gatk/gatk",bin_path2="tools/bcftoo
   dat$V2=dat$V2+1
   dat=dat %>% dplyr::mutate(Region=paste0(sub("chr","",V1),":",V2,"-",V3))
   cl=parallel::makeCluster(threads)
-  pbapply(X=dat$Region,2,FUN=vcf_mutect2,bin_path=bin_path,tumor_bam=tumor_bam,normal_bam=normal_bam,ref_genome=ref_genome,germ_resource=germ_resource,pon=pon,verbose=verbose,cl=cl)
+  pbapply(X=dat[,c("Region"),drop=FALSE],1,FUN=vcf_mutect2,bin_path=bin_path,tumor_bam=tumor_bam,normal_bam=normal_bam,ref_genome=ref_genome,germ_resource=germ_resource,pon=pon,verbose=verbose,cl=cl)
 
   if(output_dir==""){
     sep=""
