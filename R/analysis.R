e1106b89566925527b4263ec210ed67d919f27c3
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-01-21 17:06:16 +0000

    Dix

--- R/analysis.R
+++ R/analysis.R
@@ -570,6 +570,7 @@ format_ASEQ_pileup=function(file="",verbose=FALSE,output_dir=""){
   dat=read.table(file,header=TRUE)
   dat=dat %>% dplyr::select(chr,pos,dbsnp,ref,alt,A,C,G,T,RD)
   dat=dat %>% dplyr::filter(nchar(as.character(alt))>1)
+  dat=as.data.table(dat)
   dat=dat[,Value := get(as.character(alt)), by = alt]
   dat=dat %>% dplyr::mutate(af=Value/RD,cov=RD) %>% dplyr::select(chr, pos,dbsnp,ref,alt,A,C,G,T,af,cov) %>% dplyr::rename (dbsnp="rsid")
   write.table(dat,file=out_file)
