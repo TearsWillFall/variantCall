ae12e9034c03cc4ea18cda3e49a723d059af3e6e
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-01-28 13:03:35 +0000

    New function for splitting multisample vcfs

--- R/util.R
+++ R/util.R
@@ -36,3 +36,41 @@ bgzip=function(bin_path="tools/htslib/bgzip",file="",verbose=FALSE){
   }
   system(paste0(bin_path," -f ",file))
 }
+
+#' Splits multisample VCF file into multiple VCFs
+#'
+#' This function takes a multisample VCF and splits into multiple VCFS for each sample
+#'
+#' @param bin_path [Required] Path to bcftools binary. Default "tools/bcftools/bcftools"
+#' @param file [Required] Path to VCF file to split.
+#' @param verbose [Optional] Enables progress messages. Default False.
+#' @param output_dir [Optional] Path to the output directory.
+#' @export
+
+
+
+split_vcf=function(bin_path="tools/bcftools/bcftools",vcf="",verbose=FALSE,output_dir=""){
+    sep="/"
+
+    if(output_dir==""){
+      sep=""
+    }
+
+    sample_name=ULPwgs::get_sample_name(vcf)
+
+    out_file=paste0(output_dir,sep,sample_name,"_SPLIT")
+    if (!dir.exists(out_file)){
+        dir.create(out_file)
+    }
+
+  if (verbose){
+    print(paste(bin_path," query -l ",vcf))
+    apply(names,1,function(x){print(paste(bin_path, "view -c1 -Oz -s",x, "-o", paste0(x,".vcf.gz"),vcf))
+    })
+  }
+
+  names=system(paste(bin_path," query -l ",vcf), intern = TRUE, ignore.stderr = TRUE)
+  apply(names,1,function(x){system(paste(bin_path, "view -c1 -Oz -s",x, "-o", paste0(out_file,"/",x,".vcf.gz"),vcf))
+  })
+
+}
