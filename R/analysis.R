a5ba7b8cc4a5d528ff7f928a9b05f168ecc33928
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-02-15 17:44:58 +0000

    Fix

--- R/analysis.R
+++ R/analysis.R
@@ -588,7 +588,7 @@ format_SNP_data=function(bin_path="tools/bcftools/bcftools",bin_path2="tools/hts
   files3=as.data.frame(files3)
   names(files3)="BAM_path"
   files3$Sample=apply(files3,1,FUN=ULPwgs::get_sample_name)
-  files=left_join(files2,files3,by="Sample")
+  files=dplyr::left_join(files2,files3,by="Sample")
   pbapply::pbapply(FUN=function(x){
     call_ASEQ(vcf=files[x,]$VCF_path,bin_path=bin_path,bam=files[x,]$BAM_path,output_dir=output_dir,threads=1,verbose=verbose)},cl=cl)
   files3=list.files(bam_dir,recursive=TRUE,full.names=TRUE,pattern="PILEUP.ASEQ")
