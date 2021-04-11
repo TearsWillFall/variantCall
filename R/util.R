3f31a2a367a9716d991cdbcc4f135cca2afa545e
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-04-11 18:51:54 +0100

    Fix

--- R/util.R
+++ R/util.R
@@ -138,21 +138,21 @@ vcf_filter_variants=function(unfil_vcf="",bin_path="tools/bcftools/bcftools",bin
 
 
 
-    if (state!=""|grepl("%",state)){
+    if (state!=""&grepl("%",state)){
       state=paste0(" & GT[0]=\"",state,"\" ")
     }
 
-    if (ref!=""|grepl("%",ref)){
+    if (ref!=""&grepl("%",ref)){
       ref=paste0(" & ID!=\"",ref,"\" ")
     }
 
-    if (type!=""|grepl("%",type)){
+    if (type!=""&grepl("%",type)){
       type=paste0(" & TYPE=\"",type,"\" ")
     }
-    if (filter!=""|grepl("%",filter)){
+    if (filter!=""&grepl("%",filter)){
       filter=paste0(" & FILTER=\"",filter,"\" ")
     }
-    if (mq!=""|grepl("%",mq)){
+    if (mq!=""&grepl("%",mq)){
       mq=paste0("\'%MQ>",mq)
     }
 
