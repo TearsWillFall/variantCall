147c128d04373ef6a7a2084243e0cb1ac0887dc9
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-06-29 09:26:53 +0100

    Fix

--- R/analysis.R
+++ R/analysis.R
@@ -987,6 +987,8 @@ process_variants=function(bin_path="tools/ensembl-vep/vep",bin_path2="tools/ense
   vcf_sets_SNPs=vcf_sets_SNPs[grepl("SET_3",vcf_sets_SNPs)]
   vcf_sets_INDELs=vcf_sets[grepl("INDELs",vcf_sets)]
   vcf_sets_INDELs=vcf_sets_INDELs[grepl("SET_4",vcf_sets_INDELs)]
+  vcf_sets_SVs=vcf_sets[grepl("SVs",vcf_sets)]
+  vcf_sets_SVs=vcf_sets_SVs[grepl("SET_4",vcf_sets_SVs)
   set_names=c("PLATYPUS","HAPLOTYPECALLER","STRELKA2")
 
   lapply(X=vcf_sets_SNPs,FUN=function(x){
@@ -999,6 +1001,13 @@ process_variants=function(bin_path="tools/ensembl-vep/vep",bin_path2="tools/ense
     out_file_name=paste0(patient_id,".",set_names[as.numeric(ULPwgs::get_sample_name(x))+1])
     call_vep(bin_path=bin_path,bin_path2=bin_path4,bin_path3=bin_path5,vcf=x,verbose=verbose,output_dir=dirname(x),output_name=out_file_name,threads=threads);
   })
+
+  set_names=c("STRELKA","SVABA")
+  lapply(X=vcf_sets_INDELs,FUN=function(x){
+    out_file_name=paste0(patient_id,".",set_names[as.numeric(ULPwgs::get_sample_name(x))+1])
+    call_vep(bin_path=bin_path,bin_path2=bin_path4,bin_path3=bin_path5,vcf=x,verbose=verbose,output_dir=dirname(x),output_name=out_file_name,threads=threads);
+  })
+
 
 
   if (!dir.exists(paste0(out_file_dir,"/GERMLINE/HQ_SNPs/"))){
