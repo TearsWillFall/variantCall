dc1c0410ea0dbd4fe2a433a78f246d609d7b333f
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-06-29 16:03:19 +0100

    Fix

--- R/util.R
+++ R/util.R
@@ -1485,9 +1485,10 @@ generate_CLONET_sample_info=function(snp_dir="",patient_id="",output_dir=""){
     }
     out_file=paste0(patient_id,".sample.info.txt")
     files=list.files(snp_dir,full.names=TRUE,recursive=TRUE)
-    files=lapply(files,FUN=ULPwgs::get_sample_name)
     tumor=files[grepl("TumorPileup",files)]
     germ=files[grepl("NormalPileup",files)]
+    tumor=lapply(tumor,FUN=ULPwgs::get_sample_name)
+    germ=lapply(germ,FUN=ULPwgs::get_sample_name)
     sample_info=data.frame(Tumor.Array.Name="",Tumor.Bam.Name=as.vector(tumor),Normal.Array.Name="",Normal.Bam.Name=as.vector(germ))
     write.table(sample_info,file=paste0(out_file_dir,"/",out_file),quote=FALSE,row.names=FALSE,col.names=TRUE,sep="\t")
 }
