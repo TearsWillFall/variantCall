65bf62f15185a7d87f426f32f27b62bf276bfeba
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-07-01 10:20:13 +0100

    Fix

--- R/util.R
+++ R/util.R
@@ -1657,7 +1657,7 @@ plot_allelic_imbalance=function(clonet_dir="",sample_data="",output_dir="",gene_
 
 
     tissue=full_data %>% dplyr::filter(Origin!="Plasma")
-    tissue=tissue %>%dplyr::group_by(sample) %>% dplyr::mutate(Anatomy=make.unique(Anatomy,sep="_"))
+    tissue=tissue %>%dplyr::group_by(sample,chr.x,start.x,end.x) %>% dplyr::mutate(Anatomy=make.unique(Anatomy,sep="_"))
     if(dim(tissue)[1]>0){
       for (x in unique(tissue$Anatomy)){
         sub_tissue=tissue %>% dplyr::filter(Anatomy==x)
