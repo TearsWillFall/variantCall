9c9611322fac566f1ca1d20e768273780dd55095
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-07-01 12:06:26 +0100

    Fix

--- R/util.R
+++ R/util.R
@@ -1701,8 +1701,10 @@ plot_allelic_imbalance=function(clonet_dir="",sample_data="",output_dir="",gene_
     }
     full_data$ID=ifelse(full_data$Origin=="Plasma",as.character(lubridate::dmy(full_data$Timepoint_ID)),full_data$Anatomy)
     log2_corr_per_gene=full_data %>% dplyr::group_by(Symbol,ID) %>% dplyr::summarise(meanLog2corr=mean(log2.corr))
-    log2_corr_per_gene_wider=log2_corr_per_gene %>% tidyr::pivot_wider(id_cols="ID",names_from="Symbol",values_from="meanLog2corr") %>% tidyr::drop_na()
-    cor_matrix=cor(t(log2_corr_per_gene_wider))
+    log2_corr_per_gene_wider=log2_corr_per_gene %>% tidyr::pivot_wider(id_cols="ID",names_from="Symbol",values_from="meanLog2corr")
+    log2_corr_per_gene_wider =t(log2_corr_per_gene_wider)
+    log2_corr_per_gene_wider= log2_corr_per_gene_wider %>% tidyr::drop_na()
+    cor_matrix=cor(t)
     png(file ="Log2corrected_Correlation.png",width=1920,height=2500)
     corrplot(cor_matrix, order = "hclust",tl.col="black",type="lower")
     dev.off()
