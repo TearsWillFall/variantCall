0c4178c714022d6027b1b6807b9a7e5f84f49084
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-07-01 22:19:21 +0100

    Fix

--- R/util.R
+++ R/util.R
@@ -1710,7 +1710,7 @@ plot_allelic_imbalance=function(clonet_dir="",sample_data="",output_dir="",gene_
     png(file=paste0(out_file_dir,"/",unique(full_data$Patient_ID),".log2_corrected_CLONET.png"),width=1600,height=1200)
     ComplexHeatmap::Heatmap(as.matrix(log2_corr_mtx),na_col="black",column_labels=c_names,
     row_labels=r_names,column_names_gp=gpar(fontsize=7),cluster_rows=FALSE,cluster_columns=TRUE,
-    row_split=c(rep("Plasma",sum(!grepl("[a-z]",r_names))),rep("Tissue",sum(!grepl("[a-z]",r_names)))),name="log2.cor")
+    row_split=c(rep("Plasma",sum(!grepl("[a-z]",r_names))),rep("Tissue",sum(grepl("[a-z]",r_names)))),name="log2.cor")
     dev.off()
 }
 
