8a8b3c908addd5bdf2ea6b72995a9df9399d2605
Osvaldas Vainauskas <osvaldas.v@um.es>
2021-07-20 18:16:42 +0100

    Fix

--- R/analysis.R
+++ R/analysis.R
@@ -1727,12 +1727,12 @@ vcf="",verbose=FALSE,output_dir="",patient_id="",normal_id="",ref_genome=""){
 
   if(verbose){
     print(paste("perl ",bin_path, " --input-vcf ", vcf," --output-maf ",
-    paste0(out_file_dir,"/",sample_name,".vep.maf") ," --ref-genome ",ref_genome," --vep-path ",
+    paste0(out_file_dir,"/",sample_name,".vep.maf") ," --ref-fasta ",ref_genome," --vep-path ",
     vep_dir, " --vep-data ", vep_data, " --tumor-id ",sample_name,normal_id))
 
   }
   system(paste("perl ",bin_path, " --input-vcf ", vcf," --output-maf ",
-  paste0(out_file_dir,"/",sample_name,".vep.maf") ," --ref-genome ",ref_genome," --vep-path ",
+  paste0(out_file_dir,"/",sample_name,".vep.maf") ," --ref-fasta ",ref_genome," --vep-path ",
   vep_dir, " --vep-data ", vep_data, " --tumor-id ",sample_name,normal_id))
 
 }
